aa828f826f57ab7f4900f58690de4d433fc43422
TJ Bai <tjianbai@gmail.com>
2023-06-26 16:35:37 -0400

    started quick retrieve

--- api/lib/compute.py
+++ api/lib/compute.py
@@ -4,12 +4,14 @@ from typing import Dict, List, Tuple
 from annoy import AnnoyIndex
 from sentence_transformers import SentenceTransformer, CrossEncoder
 
-from models import DB, Course
+from lib.models import DB, Course
 
 MODEL = SentenceTransformer("msmarco-distilbert-base-tas-b")
 MODEL.max_seq_length = 256
-DB_PATH = "../../gen.db"
-INDEX_PATH = "../../rip/test.ann"
+DB_PATH = "./lib/gen.db"
+INDEX_PATH = "./lib/test.ann"
+
+CANDIDATE_POOL_SIZE = 100
 
 
 def compute_course_embeddings(courses: List[Course]) -> List[List[int]]:
@@ -45,13 +47,30 @@ def semantic_search(prompts: List[str]) -> List[Dict[int, Course]]:
 
     hit_maps = []
     for query_embedding in query_embds:
-        hits = u.get_nns_by_vector(query_embedding, 100)
+        hits = u.get_nns_by_vector(query_embedding, CANDIDATE_POOL_SIZE)
         hit_maps.append(create_hit_map(hits))
 
     return hit_maps
 
 
-def retrieve_rerank(prompts: List[str]) -> List[Tuple[int, Course]]:
+# def retrieve(prompts: List[str]) -> List[Tuple[int, Course]]:
+#     query_embds = compute_query_embeddings(prompts)
+#     print(f"\n>>> calcualted query embeddings {datetime.now()}")
+
+#     u = AnnoyIndex(len(query_embds[0]), "dot")
+#     u.load(INDEX_PATH)
+
+#     id_lists = [
+#         u.get_nns_by_vector(query_embedding, QUICK_POOL_SIZE)
+#         for query_embedding in query_embds
+#     ]
+
+#     id_lists_strs = [", ".join(str(num) for num in sublist) for sublist in id_lists]
+#     with DB(DB_PATH) as db:
+#         rows = db.execute()
+
+
+def retrieve_rerank(prompts: List[str]) -> List[List[Tuple[int, Course]]]:
     cross_encoder = CrossEncoder("cross-encoder/ms-marco-TinyBERT-L-2")
     semantic_cands: List[Dict[int, Course]] = semantic_search(prompts)
     print(f"\n>>> retrieved hits {datetime.now()}")
@@ -70,6 +89,24 @@ def retrieve_rerank(prompts: List[str]) -> List[Tuple[int, Course]]:
     return [[(tup[1], tup[2]) for tup in hit_list] for hit_list in res]
 
 
-if __name__ == "__main__":
-    hits = retrieve_rerank(["Feminism, suffrage movement, etc."])
-    print(hits)
+def quick_retrieve(prompt: str) -> List[Tuple[int, Course]]:
+    query_embedding = compute_query_embeddings([prompt])[0]
+
+    u = AnnoyIndex(len(query_embedding), "dot")
+    u.load(INDEX_PATH)
+
+    ids, distances = u.get_nns_by_vector(query_embedding, CANDIDATE_POOL_SIZE)
+    id_to_dist = {id: dist for id, dist in zip(ids, distances)}
+
+    with DB(DB_PATH) as db:
+        res = db.execute(
+            f"""
+            SELECT *
+            FROM Courses
+            WHERE id IN ({[str(id) for id in ids]})
+        """
+        )
+
+    res.sort(key=lambda x: id_to_dist[x[0]])
+
+    return None
--- api/main.py
+++ api/main.py
@@ -1,12 +1,36 @@
+from datetime import datetime
+
 from fastapi import FastAPI
+
+from lib.compute import retrieve_rerank, quick_retrieve
 
 app = FastAPI()
 
 
-@app.get("/test")
+@app.get("/test/")
 def read_root():
     return {
         "Field1": "Test",
         "Field2": ["1", "2", "3"],
         "Field3": {"Sub1": "a", "Sub2": "b"},
     }
+
+
+@app.get("/search/")
+def search(q: str = None):
+    if q is None:
+        return {"Error": "Query not provided"}
+
+    start_t = datetime.now()
+    courses = retrieve_rerank([q])[0]
+    end_t = datetime.now()
+
+    return {"Courses": courses, "Time": end_t - start_t}
+
+
+@app.get("/quick-search/")
+def quick_search(q: str = None):
+    if q is None:
+        return {"Error": "Query not provided"}
+
+    return quick_retrieve(q)
